97d1adc8a827b06feb12f78c325b4e61d079bf62
ykzZa <n.hryshaienko@gmail.com>
2023-08-09 22:11:54 +0300

    navigation ui hiding implemented

--- app/src/main/java/dev/ykzza/chesstimer/MainActivity.kt
+++ app/src/main/java/dev/ykzza/chesstimer/MainActivity.kt
@@ -1,7 +1,11 @@
 package dev.ykzza.chesstimer
 
+import android.R
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import dev.ykzza.chesstimer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +16,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
+
+        hideSystemUi()
+    }
+
+    private fun hideSystemUi() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window,
+            window.decorView.findViewById(R.id.content)).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
     }
 }
