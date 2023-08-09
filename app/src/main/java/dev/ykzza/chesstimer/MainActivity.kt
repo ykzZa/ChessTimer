10ccedd7dde19a26386e2795bdc6859c943f54fd
ykzZa <n.hryshaienko@gmail.com>
2023-08-09 21:46:46 +0300

    activity_main.xml edited

--- app/src/main/java/dev/ykzza/chesstimer/MainActivity.kt
+++ app/src/main/java/dev/ykzza/chesstimer/MainActivity.kt
@@ -1,14 +1,7 @@
 package dev.ykzza.chesstimer
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import dev.ykzza.chesstimer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
