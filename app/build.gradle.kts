1a9f002c2fd7c2339bb9617f596571ddf04d59ca
ykzZa <n.hryshaienko@gmail.com>
2023-08-11 21:02:12 +0300

    Room dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,15 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //Count down timer dependency
     implementation("com.github.orbitalsonic:SonicCountDownTimer:1.0.4")
+
+    //Room database dependencies
+    val roomVersion = "2.5.2"
+
+    implementation("androidx.room:room-runtime:$roomVersion")
+    annotationProcessor("androidx.room:room-compiler:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
 
 }
