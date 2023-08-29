94b7a5f2a96916768ca19c13af0bcc0ba29db5b0
ykzZa <n.hryshaienko@gmail.com>
2023-08-29 04:34:49 +0300

    RepositoryImpl update

--- app/src/main/java/dev/ykzza/chesstimer/data/ChessTimerRepositoryImpl.kt
+++ app/src/main/java/dev/ykzza/chesstimer/data/ChessTimerRepositoryImpl.kt
@@ -2,19 +2,31 @@ package dev.ykzza.chesstimer.data
 
 import android.app.Application
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.map
 import dev.ykzza.chesstimer.domain.ChessTimerRepository
 import dev.ykzza.chesstimer.domain.TimeMode
 
 class ChessTimerRepositoryImpl(
     application: Application
-): ChessTimerRepository {
+) : ChessTimerRepository {
 
     private val timeModeDao = AppDatabase.getInstance(application).timeModeDao()
+    private val dataStoreRepository = DataStoreRepository(application)
     private val mapper = TimeModeMapper()
-    override suspend fun getTimeModeById(id: Int): TimeMode {
-        val timeModeDbModel = timeModeDao.getTimeModeById(id)
-        return mapper.dbModelToEntity(timeModeDbModel)
+
+    override fun getSavedTimeMode(): LiveData<TimeMode> {
+        return dataStoreRepository.readTimeMode.asLiveData().map {
+            mapper.dbModelToEntity(it)
+        }
+    }
+
+    override suspend fun saveTimeMode(timeMode: TimeMode) {
+        dataStoreRepository.saveTimeMode(
+            id = timeMode.id,
+            baseTime = timeMode.baseTime,
+            addTime = timeMode.addTime
+        )
     }
 
     override suspend fun deleteTimeMode(id: Int) {
